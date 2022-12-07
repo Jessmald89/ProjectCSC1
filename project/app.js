31fa366ad00005f04d4712793def5a8b57250751
Jessmald89 <jamaldonado2@my.waketech.edu>
2022-12-07 16:28:30 -0500

    initial

---
+++ project/app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+// routes
+app.get('/ProjectCSC', (req, res)=> {
+
+  res.sendFile(__dirname + '/public/index.html');
+})
+
+app.post('/ProjectCSCPost',(req,res) =>{
+  console.log(req.body);
+
+})
+
+
+app.listen(8080, function () {
+  console.log('Sever is listening on port 8080');
+});
+
+module.exports = app;
